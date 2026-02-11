f7d4467e8648c61a09753a8675873f4d9cba8fcb
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-02-11 17:37:48 -0300

    Tela de Login

--- src/pages/Cadastro/styles.js
+++ src/pages/Cadastro/styles.js
@@ -4,15 +4,55 @@ const styles = StyleSheet.create({
    container: {
     backgroundColor: "#ffffff",
     flex: 1,
-    alignItems: 'center',
     flexDirection: 'column'
    },
    banner: {
-    width: 30,
-    height: 30,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    padding: 10,
+    alignItems: 'center',
+    gap: 10,
+    flex: 0.2
    },
+
+   form: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    justifyContent: 'start',
+    margin: 20,
+    gap: 10,
+    flex: 0.70
+   },
+   input: {
+    fontSize: 30,
+    padding: 4,
+    borderColor: '#ccc',
+    color: '#7a7a7a',
+    borderRadius: 10,
+    borderWidth: 2,
+   },
+   button: {
+    backgroundColor: '#f57e42',
+    borderRadius: 10,
+    marginLeft: '20%',
+    marginRight: '20%',
+    margin: '10%',
+    text: {
+        color: '#fff',
+        padding: 10,
+        fontSize: 32,
+        textAlign: 'center'
+    }
+   },
+
    h1: {
-    fontSize: 35,
+    color: '#ffffff',
+    fontSize: 25,
+    textCenter: {
+        color: '#ffffff',
+        fontSize: '200%',
+        textAlign: 'center'
+    }
    }
 });
 export default styles;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,34 @@
-import { View, Text, TextInput, Image } from "react-native";
+import { View, Text, TextInput, Image, Pressable } from "react-native";
 import styles from "../Cadastro/styles";
-
+import { useNavigation } from "@react-navigation/native";
 export default function Login() {
+    const navigate = useNavigation()
     return (
         <View style={styles.container}>
-            <View>
-                <Text style={styles.h1}>Login</Text>
-                <Image source={require('bootstrap-icons/icons/bank2.svg')} style={styles.banner} />
+            <View style={styles.banner}>
+                <Text style={styles.h1.textCenter}>Login</Text>
+                <View style={{backgroundColor: '#ffffff', padding: 20, borderRadius: '50%'}}>
+                <Image style={{
+     textAlign: 'center', width: 70, height: 70,
+                }} source={require("bootstrap-icons/icons/bank2.svg")} />
+                </View>
             </View>
-            <View>
-                <TextInput placeholder="Digite seu email" />
+            <View style={styles.form}>
+               
+                <TextInput style={styles.input} placeholder="E-Mail" />
+                <TextInput style={styles.input} placeholder="Senha" />
+                
+                <Pressable style={styles.button}>
+                    <Text style={styles.button.text}>Logar</Text>
+                </Pressable>
+
+                <View>
+                <Pressable onPress={()=>navigate.navigate("Cadastro")}>
+                    <Text style={{color: '#f57e42', textAlign: 'center', fontSize: 20}}>Não tem uma conta? Cadastre-se</Text>
+                </Pressable>
+                </View>
+                
+            
             </View>
         </View>
     )
