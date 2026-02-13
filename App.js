d104e8ac9be28acf9ae31b8efe359c20b780398c
Samu Mene <samuelsantiago2222@gmail.com>
2026-02-12 23:11:37 -0300

    NativeWind no projeto

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import Login from './src/pages/Login/Login';
+import './global.css';
 import Cadastro from './src/pages/Cadastro/Cadastro';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,35 +1,11 @@
 import { View, Text, TextInput, Image, Pressable } from "react-native";
-import styles from "../Cadastro/styles";
 import { useNavigation } from "@react-navigation/native";
 export default function Login() {
     const navigate = useNavigation()
     return (
-        <View style={styles.container}>
-            <View style={styles.banner}>
-                <Text style={styles.h1.textCenter}>Login</Text>
-                <View style={{backgroundColor: '#ffffff', padding: 20, borderRadius: '50%'}}>
-                <Image style={{
-     textAlign: 'center', width: 70, height: 70,
-                }} source={require("bootstrap-icons/icons/bank2.svg")} />
-                </View>
-            </View>
-            <View style={styles.form}>
-               
-                <TextInput style={styles.input} placeholder="E-Mail" />
-                <TextInput style={styles.input} placeholder="Senha" />
-                
-                <Pressable style={styles.button}>
-                    <Text style={styles.button.text}>Logar</Text>
-                </Pressable>
-
-                <View>
-                <Pressable onPress={()=>navigate.navigate("Cadastro")}>
-                    <Text style={{color: '#f57e42', textAlign: 'center', fontSize: 20}}>Não tem uma conta? Cadastre-se</Text>
-                </Pressable>
-                </View>
-                
-            
-            </View>
+        <View className="flex-1 justify-around bg-orange-400">
+            <Text className="text-2xl font-bold">Olá Mundo!</Text>
+            <Text>Outro texto</Text>
         </View>
     )
 }
