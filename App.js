a546675df64932b33c89310ba50c33a3a197c025
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-02-25 16:08:39 -0300

    Tela de Splash

--- App.js
+++ App.js
@@ -4,12 +4,16 @@ import Cadastro from './src/pages/Cadastro/Cadastro';
 import Home from './src/pages/Home/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Splash from './src/pages/Splash/Splash';
 const Stack = createStackNavigator();
 export default function App() {
   
   return (
     <NavigationContainer>
       <Stack.Navigator >
+        <Stack.Screen name='Splash' component={Splash} options={{
+          headerShown: false
+        }} />
         <Stack.Screen name='Login' component={Login} options={{
           headerShown: false
         }}/>
---
+++ src/pages/Splash/Splash.js
@@ -0,0 +1,31 @@
+import { useNavigation } from "@react-navigation/native"
+import { useEffect } from "react";
+import { View, Image, ActivityIndicator } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+export default function Splash() {
+    const navigate = useNavigation();
+     async function isLogin() {
+        const login = await AsyncStorage.getItem("@login");
+        if(login!=null) {
+
+        } else {
+            navigate.navigate("Login");
+        }
+     }
+    useEffect(()=>{
+        setTimeout(()=>{
+            isLogin();
+        }, 2000)
+    }, [])
+
+    return (
+        <View style={{flex: 1}} className="justify-center bg-teal-600">
+            <View style={{flex:0.5}} className="items-center justify-end p-3">
+                <Image style={{width: "100%", height:"50%"}} resizeMode="contain" tintColor={"#ffffff"} source={require('../../../assets/img/btg-logo.svg')} />
+            </View>
+            <View style={{flex:0.5}} className="items-center justify-start">
+                <ActivityIndicator size="large" color="#ffffff" />
+            </View>
+        </View>
+    )
+}
